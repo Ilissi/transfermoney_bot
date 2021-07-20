c1f10fa8194a99c5f232b822bc57e5127837cd21
Виктор Кузьминский <maksimboginic@Maksims-MacBook-Pro.local>
2021-07-20 22:41:36 +0300

    add webroutes

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
 from aiogram import executor
 
+from aiogram.dispatcher.webhook import configure_app
 from loader import dp
 import middlewares, filters, handlers
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
 from utils.db_api.database import create_db
+from aiohttp import web
+
+
+async def api_handler(request):
+    return web.json_response({"status": "OK"}, status=200)
 
 
 async def on_startup(dispatcher):
     # Устанавливаем дефолтные команды
     await set_default_commands(dispatcher)
     res = await create_db()
-    print(res)
+    app = web.Application()
+
+    app.add_routes([web.post('/api', api_handler)])
+    configure_app(dp, app, "/bot")
     # Уведомляет про запуск
     await on_startup_notify(dispatcher)
 
